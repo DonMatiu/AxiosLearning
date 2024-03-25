8fd9529f74fa1ea7485fd22c910aa7e070fbf72a
DonMatiu <98232499+DonMatiu@users.noreply.github.com>
2024-03-25 10:17:48 -0300

    Initial Setup + gitignore added

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});
